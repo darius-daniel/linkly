98fc07377155a5d9d17239e3c0d1217984edffab
Darius Daniel <dariusdaniel771@email.com>
2024-08-21 13:26:30 +0100

    Remove directory for kindeAuth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,5 @@
 'use client';
-// import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 
 export default function Page({ params }: { params: { slug: string } }) {
-  // const { user } = useKindeBrowserClient();
-  // console.log({ user });
   return <></>;
 }
