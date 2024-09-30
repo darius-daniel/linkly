6518cecdaf8f98d0eabdfc6415b5123e4992d711
Darius Daniel <dariusdaniel771@email.com>
2024-09-30 15:23:55 +0100

    Changed pagination style and moved pagination code to its own component

---
+++ app/ui/components/pagination.tsx
@@ -0,0 +1,57 @@
+import { getShortLinkLastPageNum } from '@/app/lib/actions';
+import { User } from '@prisma/client';
+import { ChevronsLeft, ChevronsRight } from 'lucide-react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+interface PaginationProps {
+  user: {
+    id: string;
+    email: string | null;
+    family_name: string | null;
+    given_name: string | null;
+    picture: string | null;
+  } | null;
+  currentPage: {
+    value: number;
+    set: Dispatch<SetStateAction<number>>;
+  };
+}
+
+export default function Pagination({ currentPage, user }: PaginationProps) {
+  const [lastPageNum, setLastPageNum] = useState<number | undefined>();
+
+  useEffect(() => {
+    if (user) {
+      getShortLinkLastPageNum(user.id, 10).then((lastPage) => {
+        console.log('Last page: ', lastPage);
+        setLastPageNum(lastPage);
+      });
+    }
+  }, [currentPage.value]);
+
+  return (
+    <div className="bg-transparent justify-center mt-1 text-center">
+      <div className="join bg-custom-dark-gray size-fit">
+        <button
+          className="join-item btn btn-sm bg-inherit"
+          onClick={() => currentPage.set(currentPage.value - 1)}
+          disabled={currentPage.value - 1 < 1}
+        >
+          <ChevronsLeft size={16} />
+        </button>
+        <button className="join-item btn btn-sm btn-active">
+          {`Page ${currentPage.value}`}
+        </button>
+        <button
+          className="join-item btn btn-sm bg-inherit"
+          onClick={() => currentPage.set(currentPage.value + 1)}
+          disabled={
+            lastPageNum === undefined || currentPage.value + 1 > lastPageNum
+          }
+        >
+          <ChevronsRight size={16} />
+        </button>
+      </div>
+    </div>
+  );
+}
