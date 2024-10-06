c5c54710e0d972675424106ad53a1b11e81ceaf4
Darius Daniel <dariusdaniel771@email.com>
2024-10-06 01:24:25 +0100

    Logging values to the console for debugging

--- app/ui/components/pagination.tsx
+++ app/ui/components/pagination.tsx
@@ -31,17 +31,17 @@ export default function Pagination({ currentPage, user }: PaginationProps) {
     <div className="bg-transparent justify-center mt-1 text-center">
       <div className="join bg-custom-dark-gray size-fit">
         <button
-          className="join-item btn btn-sm bg-inherit text-custom-lite"
+          className="join-item btn btn-sm bg-inherit text-custom-lite hover:text-custom-dark-gray"
           onClick={() => currentPage.setValue(currentPage.value - 1)}
           disabled={currentPage.value - 1 < 1}
         >
           <ChevronsLeft size={16} />
         </button>
-        <button className="join-item btn btn-sm btn-active bg-custom-dark-gray">
+        <button className="join-item btn btn-sm btn-active bg-custom-dark-gray text-custom-lite">
           {`Page ${currentPage.value}`}
         </button>
         <button
-          className="join-item btn btn-sm bg-inherit text-custom-lite"
+          className="join-item btn btn-sm bg-inherit text-custom-lite hover:text-custom-dark-gray"
           onClick={() => currentPage.setValue(currentPage.value + 1)}
           disabled={
             lastPageNum === undefined || currentPage.value + 1 > lastPageNum
