577a0e2cf200cb84da0d32a8c1e1306cdc74167c
Darius Daniel <dariusdaniel771@gmail.com>
2025-03-18 16:14:01 +0100

    style: refine UI components layout and spacing

--- app/ui/components/link-input.tsx
+++ app/ui/components/link-input.tsx
@@ -16,19 +16,23 @@ export default function LinkInput({ user }: LinkInputProps) {
 
   return (
     <form action={formAction} className="flex flex-row relative">
-      <Link className="absolute left-4 top-4" size={16} />
-      <label className="input input-bordered w-full focus:border focus:border-custom-blue hover:outline-none flex items-center ps-12 rounded-full relative">
-        <input type="text" className="grow" placeholder="Enter your link" name="url" />
-        <button
-          type="submit"
-          className="btn bg-custom-blue hover:bg-blue-900 opacity-70 text-nowrap rounded-full text-white relative left-4"
-        >
-          <span className="hidden lg:block">Shorten Link</span>
-          <ArrowRight className="lg:hidden" />
-        </button>
-        {state?.error && <p className="text-custom-pink text-sm">{state.error.url}</p>}
-        {state?.message && <p className="text-custom-pink text-sm">{state.message}</p>}
-      </label>
+      <div className="flex flex-col gap-2">
+        <div>
+          <Link className="absolute left-4 top-4" size={16} />
+          <label className="input input-bordered w-full focus:border focus:border-custom-blue hover:outline-none flex items-center ps-12 rounded-full relative">
+            <input type="text" className="grow" placeholder="Enter your link" name="url" />
+            <button
+              type="submit"
+              className="btn bg-custom-blue hover:bg-blue-900 opacity-70 text-nowrap rounded-full text-white relative left-4"
+            >
+              <span className="hidden lg:block">Shorten Link</span>
+              <ArrowRight className="lg:hidden" />
+            </button>
+          </label>
+        </div>
+        {state?.error && <p className="text-custom-pink text-xs">{state.error.url}</p>}
+        {state?.message && <p className="text-custom-pink text-xs">{state.message}</p>}
+      </div>
     </form>
   );
 }
